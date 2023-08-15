f9db53a4e39fd010cd6d97f64f2ad38b72d49ee2
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-08-15 20:13:49 -0300

    up

--- src/controllers/usuarios_table.js
+++ src/controllers/usuarios_table.js
@@ -2,17 +2,6 @@
     const database = require('../config/db.js');
     const Usuario = require('../models/usuarioModel.js');
     await database.sync();
-
-    // const novoUsuario = await Usuario.create({
-    //     NOME: 'Helena',
-    //     CPF: '11333244455',
-    //     EMAIL: 'helena@helena.com',
-    //     TEL: '21981915682',
-    //     DATA_NASCIMENTO: '2013-12-13 13:45:00',
-    //     IDADE: 30
-    // });
-    // console.log(novoUsuario);
-    
 }) ();
 
 //comando sync()vai verificar os modelos que eu tenho no meu projeto para com as tabelas que eu tenho la no meu db, e vai garantir que os dois estejam iguais, e se ela n existi criar ela automaticamente.
