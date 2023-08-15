c52efa551dfb795fa545020028bc95aafefd0180
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-08-15 11:04:12 -0300

    criar usuarios direto

--- src/controllers/usuarios_table.js
+++ src/controllers/usuarios_table.js
@@ -3,25 +3,15 @@
     const Usuario = require('../models/usuarioModel.js');
     await database.sync();
 
-    // const novoUsuario = await Produto.create({
-    //     nome: 'Fone',
-    //     preco: 200,
-    //     descricao: 'Fone bacana.'
+    // const novoUsuario = await Usuario.create({
+    //     NOME: 'camila',
+    //     CPF: '11122233355',
+    //     EMAIL: 'camila@camila.com',
+    //     TEL: '21969066303',
+    //     DATA_NASCIMENTO: '1994-06-25 02:00:00',
+    //     IDADE: 29
     // });
-    // console.log(novoProduto);
-
-    // operação de leitura(R do CRUD)
-    // const produtos = await Produto.findAll(); //ler todos
-
-    // const produto = await Produto.findByPk(1); //ler o ID
-    // console.log(produto);
-
-    //U do CRUD
-    // produto.descricao = "Fiz uma alteração";
-    // await produto.save();
-
-    //D do CRUD
-    // await produto.destroy();
+    // console.log(novoUsuario);
     
 }) ();
 
